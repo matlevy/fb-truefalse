a8ab31e3cbe02ea820d84aa8a08d77519568eba0
Matthew Levy <matn53@gmail.com>
2018-11-12 22:48:46 +0000

    users can now specify question amount

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,7 @@
 import * as functions from 'firebase-functions';
 import { 
     dialogflow, 
-    DialogflowConversation,
-    RichResponse
+    DialogflowConversation
 } from "actions-on-google";
 import { OpenTriviaResponse } from './openTrivia';
 
@@ -14,37 +13,63 @@ const welcome = (conv:DialogflowConversation) => {
     console.log('Quiz opened');
 }
 
-const askQestion = (conv:DialogflowConversation, params) => {
+const poseQuestion = (conv:DialogflowConversation) => {
+    const convData: any = conv.data;
+    const q = convData.questions.pop();
+    convData.answer = (q.correct_answer === "True");
+    conv.ask(`<speak>True or False, ${decodeURI(q.question)}.</speak>`);
+}
+
+const askQuestion = (conv:DialogflowConversation, params) => {
     const difficulty = params.questionDifficulty;
     const convData: any = conv.data;
-    console.log(params);
-    if(convData && convData.lastCorrect!==undefined){
-        conv.add(`<speak><p>${convData.lastCorrect?'Correct!':'Incorrect!'}</p></speak>`);
-    };
-    return fetch('https://opentdb.com/api.php?amount=1&type=boolean')
-        .then((response) => {
-            if (response.status < 200 || response.status >= 300) {
-                throw new Error(response.statusText);
-            } else {
-                return response.json();
-            }
-        })
-        .then((response:OpenTriviaResponse) => {
-            const q = response.results[0];
-            convData.answer = (q.correct_answer === "True");
-            conv.ask(`<speak>True or False, ${decodeURI(q.question)}.</speak>`);
-        })
+    const quantity = params.questionQuantity ? params.questionQuantity : 1;
+    if(convData.questions===undefined) {
+        convData.questions = [];
+    }
+    console.log(params, convData);
+    if(convData.questions.length===0) {
+        return fetch(`https://opentdb.com/api.php?amount=${quantity}&type=boolean`)
+            .then((response) => {
+                if (response.status < 200 || response.status >= 300) {
+                    throw new Error(response.statusText);
+                } else {
+                    return response.json();
+                }
+            })
+            .then((response:OpenTriviaResponse) => {
+                convData.questions = response.results;
+                poseQuestion(conv);
+            })
+    } else {
+        if(convData && convData.lastCorrect!==undefined){
+            conv.add(`<speak><p>${convData.lastCorrect?'Correct!':'Incorrect!'}</p></speak>`);
+        };
+        poseQuestion(conv);
+    }
 }
 
 const checkAnswer = (conv:DialogflowConversation, params: any) => {
     const convData: any = conv.data;
     console.log(params);
     convData.lastCorrect = (params.answerTrueFalse === convData.answer.toString());
-    conv.followup('questionASK');
+    if(convData.questions.length>0) {
+        conv.followup('questionASK');
+    } else {
+        conv.followup('questionsComplete');
+    }
+}
+
+const quizComplete = (conv:DialogflowConversation, params: any) => {
+    console.log('Quiz complete: ', params);
+    const convData: any = conv.data;
+    conv.add(`<speak><p>${convData.lastCorrect?'Correct!':'Incorrect!'}</p></speak>`);
+    conv.close('Thank you for playing!');
 }
 
 app.intent('welcome', welcome);
-app.intent('question.ASK', askQestion);
+app.intent('question.ASK', askQuestion);
 app.intent('question.ANSWER', checkAnswer);
+app.intent('quiz.COMPLETE', quizComplete);
 
 export const trueOrFalse = functions.https.onRequest(app);
